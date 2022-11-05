7d71cdd8305057582e2b2de474b47c7f640d60a3
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-05 08:39:50 -0500

    Completed comment skeleton plan

---
+++ public/client.js
@@ -0,0 +1,26 @@
+//on document load run onReady
+
+//onReady Function
+//render with GET that can be reused
+//listen for operator click, run operatorCApture set as client variable
+//listen for equals click, run calculateInputs
+//listen for clear button click run render?
+
+//render function
+//clear history
+//GET function, /calculate
+//will need to return history AND answer
+//response will be used to append to DOM
+//clear inputs (can be used for clear button too)
+
+//global variable = operator
+
+//operatorCapture
+// operator = $(this).text ();
+
+//calculateInputs
+//ajax POST request, /calculate
+// data includes operator capture and inputs
+//returns response of status code
+//then GET request of render
+//dont forget the catch statements
---
+++ server/server.js
@@ -0,0 +1,24 @@
+//Set the Port
+//Start Express
+//Start Body Parser
+
+//GLOBAL VARIABLES
+
+//let calculatorHistory = [];
+//let returnData = [];
+
+//GET and POST and DELETE FUNCTIONS
+
+//POST /calculate
+//push data object into the calculatorHistory array
+//app.sendStatus(200);
+
+// GET /calculate
+//set returnData to empty array
+//grab the information from the calculatorHistory.length-1 index
+//evaluate the answer without eval
+//push answer to the 0 index of returnData
+//loop over calculator history, push into return data
+//response is returnData
+
+//PROCESSING FUNCTIONS
